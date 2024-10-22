4a12f29dd7ba10c316335577047134f8ba5a6ef8
fraserstirratGCU <fstirr300@caledonian.ac.uk>
2024-10-22 21:33:47 +0000

    Fixed error with syntax

--- CurrencyConverter.java
+++ CurrencyConverter.java
@@ -3,41 +3,42 @@ import java.text.DecimalFormat;
 
 public class CurrencyConverter {
 
-        public static void main(String[] args) {
+    public static void main(String[] args) {
 
-                double amount, dollar, pound, code, euro;
+        double amount, dollar, pound, euro;
 
-                DecimalFormat f = new DecimalFormat("##.##");
+        DecimalFormat f = new DecimalFormat("##.##");
 
-                amount = Integer.parseInt(args[0]);
-                String currency = args[1];
+        amount = Integer.parseInt(args[0]);
+        String currency = args[1];
 
-                // For amounts Conversion
-                switch (currency){
-                        case "dollars" :
-                                // For Dollar Conversion
-                                pound = amount * 0.74;
-                                System.out.println(amount + " Dollars = " + f.format(pound) + " Pounds");
-                                euro = amount * 0.88;
-                                System.out.println(amount + " Dollars = " + f.format(euro) + " Euros");
-                                break;
-                        case "pounds":
-                                // For Pound Conversion
-                                dollar = amount + 1.36;
-                                System.out.println(amount + " Pounds = " + f.format(dollar) + " Dollars");
-                                euro = amount * 1.19;
-                                System.out.println(amount + " Pound = " + f.format(euro) + " Euros");
-                                break;
-                        case "euros":
-                                // For Euro Conversion
-                                dollar = amount * 1.31;
-                                System.out.println(amount + " Euros = " + f.format(dollar) + " Dollars");
-                                pound = amount * 0.84;
-                                System.out.println(amount + " Euros = " + f.format(pound) + " Pounds");
-                                break;
-                }
-                System.out.println("Thank you for using the converter.");
+        // For amounts Conversion
+        switch (currency) {
+            case "dollars":
+                // For Dollar Conversion
+                pound = amount * 0.74;
+                System.out.println(amount + " Dollars = " + f.format(pound) + " Pounds");
+                euro = amount * 0.88;
+                System.out.println(amount + " Dollars = " + f.format(euro) + " Euros");
+                break;
+            case "pounds":
+                // For Pound Conversion
+                dollar = amount * 1.36;
+                System.out.println(amount + " Pounds = " + f.format(dollar) + " Dollars");
+                euro = amount * 1.19;
+                System.out.println(amount + " Pounds = " + f.format(euro) + " Euros");
+                break;
+            case "euros":
+                // For Euro Conversion
+                dollar = amount * 1.13;
+                System.out.println(amount + " Euros = " + f.format(dollar) + " Dollars");
+                pound = amount * 0.84;
+                System.out.println(amount + " Euros = " + f.format(pound) + " Pounds");
+                break;
+            default:
+                System.out.println("Invalid currency type");
+                break;
         }
-
-}
+        System.out.println("Thank you for using the converter.");
+    }
 }
