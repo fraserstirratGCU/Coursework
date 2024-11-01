fa4bdd2312af4c25a4957795d0db24a07a76d8e9
fraserstirratGCU <fstirr300@caledonian.ac.uk>
2024-11-01 16:06:04 +0000

    Removed some duplicate code, stored the conversion rates to improve maintainability and made the code cleaner - hopefully fixing some sonar-qube issues

--- CurrencyConverter.java
+++ CurrencyConverter.java
@@ -4,13 +4,24 @@ import java.text.DecimalFormat;
 public class CurrencyConverter {
 
     public static void main(String[] args) {
+  import java.text.DecimalFormat;
+
+public class CurrencyConverter {
+
+    private static final double DOLLAR_TO_POUND = 0.74;
+    private static final double DOLLAR_TO_EURO = 0.88;
+    private static final double POUND_TO_DOLLAR = 1.36;
+    private static final double POUND_TO_EURO = 1.19;
+    private static final double EURO_TO_DOLLAR = 1.13;
+    private static final double EURO_TO_POUND = 0.84;
+
+    public static void main(String[] args) {
         if (args.length < 2) {
             System.out.println("Error: Please provide an amount and a currency type.");
             return;
         }
 
-        double amount, dollar, pound, euro;
-
+        double amount;
         try {
             amount = Double.parseDouble(args[0]);
         } catch (NumberFormatException e) {
@@ -19,37 +30,34 @@ public class CurrencyConverter {
         }
 
         String currency = args[1].toLowerCase(); // Convert currency to lowercase
+        convertCurrency(amount, currency);
+        System.out.println("Thank you for using the converter.");
+    }
 
+    private static void convertCurrency(double amount, String currency) {
         DecimalFormat f = new DecimalFormat("##.##");
-
-        // For amounts Conversion
         switch (currency) {
             case "dollars":
-                // For Dollar Conversion
-                pound = amount * 0.74;
-                System.out.println(amount + " Dollars = " + f.format(pound) + " Pounds");
-                euro = amount * 0.88;
-                System.out.println(amount + " Dollars = " + f.format(euro) + " Euros");
+                printConversion(amount, "Dollars", f.format(amount * DOLLAR_TO_POUND), "Pounds");
+                printConversion(amount, "Dollars", f.format(amount * DOLLAR_TO_EURO), "Euros");
                 break;
             case "pounds":
-                // For Pound Conversion
-                dollar = amount * 1.36;
-                System.out.println(amount + " Pounds = " + f.format(dollar) + " Dollars");
-                euro = amount * 1.19;
-                System.out.println(amount + " Pounds = " + f.format(euro) + " Euros");
+                printConversion(amount, "Pounds", f.format(amount * POUND_TO_DOLLAR), "Dollars");
+                printConversion(amount, "Pounds", f.format(amount * POUND_TO_EURO), "Euros");
                 break;
             case "euros":
-                // For Euro Conversion
-                dollar = amount * 1.13;
-                System.out.println(amount + " Euros = " + f.format(dollar) + " Dollars");
-                pound = amount * 0.84;
-                System.out.println(amount + " Euros = " + f.format(pound) + " Pounds");
+                printConversion(amount, "Euros", f.format(amount * EURO_TO_DOLLAR), "Dollars");
+                printConversion(amount, "Euros", f.format(amount * EURO_TO_POUND), "Pounds");
                 break;
             default:
                 System.out.println("Error: Wrong currency type.");
                 break;
         }
-        System.out.println("Thank you for using the converter.");
+    }
+
+    private static void printConversion(double amount, String fromCurrency, String convertedAmount, String toCurrency) {
+        System.out.println(amount + " " + fromCurrency + " = " + convertedAmount + " " + toCurrency);
     }
 }
+
 
